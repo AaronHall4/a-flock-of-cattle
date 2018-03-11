3c6fb49cf8c334a8b485cb9c0d42012352781afa
supercavitation <mototerminator101@gmail.com>
2018-03-11 12:19:18 -0400

    Add files via upload

--- character creation.py
+++ character creation.py
@@ -170,23 +170,6 @@ def characterCreation(recRace, recClass):
 			elif userRace.lower()=='other':
 				userRace=str(input('Would you like to be a dwarf, elf, halfling, human, dragonborn, gnome, half-elf, half-orc, or tiefling? '))
 				userRace=userRace.lower()
-	userClass=str(input('Would you like to be a',recClass,'or would you like to choose another race? (Yes/Other) '))
-	if userClass.lower()=='yes':
-		userClass=recClass
-	elif userClass.lower()=='other':
-		userClass=str(input('Would you like to be a barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, or wizard? '))
-		userClass=userClass.lower()
-		while userClass not in classList:
-			userClass=str(input('Would you like to be a barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, or wizard? '))
-			userClass=userClass.lower()
-	else:
-		while userClass not in classList:
-			userClass=str(input('Would you like to be a',recClass,'or would you like to choose another class? (Yes/Other) '))
-			if userClass.lower()=='yes':
-				userClass=recClass
-			elif userClass.lower()=='other':
-				userClass=str(input('Would you like to be a barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, or wizard? '))
-				userClass=userClass.lower()
 	proficiences=[]
 	attributes=[]
 	languages=[]
@@ -288,6 +271,323 @@ def characterCreation(recRace, recClass):
 		walkingSpeed=30
 		attributes+=['Darkvision','Hellish Resistance']
 		cantrips+=['thaumaturgy']
-		languages+=['Common','Infernal']
+		languages+=['Common','Infernal']	
+	userClass=str(input('Would you like to be a',recClass,'or would you like to choose another race? (Yes/Other) '))
+	if userClass.lower()=='yes':
+		userClass=recClass
+	elif userClass.lower()=='other':
+		userClass=str(input('Would you like to be a barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, or wizard? '))
+		userClass=userClass.lower()
+		while userClass not in classList:
+			userClass=str(input('Would you like to be a barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, or wizard? '))
+			userClass=userClass.lower()
+	else:
+		while userClass not in classList:
+			userClass=str(input('Would you like to be a',recClass,'or would you like to choose another class? (Yes/Other) '))
+			if userClass.lower()=='yes':
+				userClass=recClass
+			elif userClass.lower()=='other':
+				userClass=str(input('Would you like to be a barbarian, bard, cleric, druid, fighter, monk, paladin, ranger, rogue, sorcerer, warlock, or wizard? '))
+				userClass=userClass.lower()
+	equipment=[]
+	if userClass=='barbarian':
+		hitDice=12
+		hpMax+=12
+		if subType!='mountain'):
+			proficiences+=['light armor','medium armor']
+		proficiences+=['shields','simple weapons','martial weapons']
+		savingThrows=[0,2]
+		skillOne=str(input('Choose one of the following: Animal Handling, Intimidation, Nature, Perception, and Survival '))
+		skillTwo=str(input('Choose another one of those skills'))
+		while skillOne.lower()==skillTwo.lower():
+			skillTwo=str(input('Choose a different one of those skills'))
+		proficiences+=[skillOne,skillTwo]
+		print('For the next few options, enter 1 to choose the first option, and 2 to choose the second.')
+		equipmentChoice=input('Would you like a greataxe or any martial melee weapon?')
+		if equipmentChoice==1:
+			equipment+=['greataxe']
+		else:
+			equipment+=['any martial melee weapon']
+		equipmentChoice=input('Would you like two handaxes or any simple weapon?')
+		if equipmentChoice==1:
+			equipment+=['2 handaxes']
+		else:
+			equipment+=['any simple weapon']
+		equipment+=['explorer\'s pack','four javelins']
+		attributes+=['2 rages','unarmored defense']
+	if userClass=='bard':
+		hitDice=8
+		hpMax+=8
+		if 'light armor' not in proficiences:
+			proficiences+=['light armor']
+		proficiences+='simple weapons'
+		if 'hand crossbow' not in proficiences:
+			proficiences+=['hand crossbow']
+		if 'longsword' not in proficiences:
+			proficiences+=['longsword']
+		if 'rapier' not in proficiences:
+			proficiences+=['rapier']
+		if 'shortsword' not in proficiences:
+			proficiences+=['shortsword']
+		savingThrows=[1,5]
+		proficiences+=[str(input('Choose a musical instrument to become proficient in:'))]
+		proficiences+=[str(input('Choose another musical instrument to become proficient in:'))]
+		proficiences+=[str(input('Choose a third musical instrument to become proficient in:'))]
+		skillOne=str(input('Choose a skill you\'re not already proficient in: '))
+		skillTwo=str(input('Choose a different skill you\'re not already proficient in: '))
+		skillThree=str(input('Choose a third skill you\'re not already proficient in '))
+		print('For the next few options, enter 1 to choose the first option, 2 to choose the second, and 3 to choose the third if there is one.')
+		equipmentChoice=input('Would you like a rapier, a longsword, or any simple weapon?')
+		if equipmentChoice==1:
+			equipment+=['rapier']
+		elif equipmentChoice==2:
+			equipment+=['longsword']
+		else:
+			equipment+=['any simple weapon']
+		equipmentChoice=input('Would you like a diplomat\'s pack or and entertainer\'s pack?')
+		if equipmentChoice==1:
+			equipment+=['diplomat\'s pack']
+		else:
+			equipment+=['entertainer\'s pack']
+		equipmentChoice=input('Would you like a lute or another musical instrument?')
+		if equipmentChoice==1:
+			equipment+=['lute']
+		else:
+			equipment+=['any musical instrument']
+		equipment+=['leather armor','dagger']
+	if userClass=='cleric':
+		hitDice=8
+		hpMax+=8
+		if 'light armor' not in proficiences:
+			proficiences+=['light armor']
+		if 'medium armor' not in proficiences:
+			proficiences+=['medium armor']
+		proficiences+=['shields']
+		proficiences+=['all simple weapons']
+		savingThrows=[3,5]
+		skillOne=str(input('Choose a skill: History, Insight, Medicine, Persuasion, or Religion '))
+		skillTwo=str(input('Choose another skill from above:'))
+		while skillTwo.lower()==skillOne.lower():
+			skillTwo=str(input('Choose another skill from above:'))
+		proficiences+=[skillOne,skillTwo]
+		print('For the next few options, enter 1 to choose the first option, and 2 to choose the second.')
+		equipmentChoice=input('Would you like a Mace or a Warhammer (choose warhammer only if you\'re a Dwarf) ')
+		if equipmentChoice==1:
+			equipment+=['mace']
+		else:
+			equipment+=['warhammer']
+		equipmentChoice=input('Would you like scale mail or leather armor?')
+		if equipmentChoice==1:
+			equipment+=['scale mail']
+		else:
+			equipment+=['leather armor']
+		equipmentChoice=input('Would you like a light crossbow and 20 bolts or any simple weapon? ')
+		if equipmentChoice==1:
+			equipment+=['light crossbow and 20 bolts']
+		else:
+			equipment+=['any simple weapon']
+		equipmentChoice=input('Would you like a priest\'s pack or an explorer\'s pack? ')
+		if equipmentChoice==1:
+			equipment+=['priest\'s pack']
+		else:
+			equipment+=['explorer\'s pack']
+		equipment+=['shield','holy symbol']
+	if userClass=='druid':
+		hitDice=8
+		hpMax+=8
+		if 'light armor' not in proficiences:
+			proficiences+=['light armor']
+		if 'medium armor' not in proficiences:
+			proficiences+=['medium armor']
+		proficiences+=['shield','club','dagger','dart','javelin','mace','quarterstaff','scimitar','sickle','sling','spear','herbalism kit']
+		savingThrows=[3,4]
+		skillOne=str(input('Choose a skill from the following: Arcana, Animal Handling, Insight, Medicine, Nature, Perception, Religion, Survival'))
+		skillTwo=str(input('Choose a different skill from the above: '))
+		proficiences+=[skillOne,skillTwo]
+		print('For the next few options, enter 1 to choose the first option, and 2 to choose the second.')
+		equipmentChoice=input('Would you like a wooden shield or any simple weapon? ')
+		if equipmentChoice==1:
+			equipment+=['wooden shield']
+		else:
+			equipment+=['any simple weapon']
+		equipmentChoice=input('Would you like a scimitar or any simple melee weapon?')
+		if equipmentChoice==1:
+			equipment+=['scimitar']
+		else:
+			equipment+=['any simple melee weapon']
+		equipment+=['leather armor','explorer\'s pack', 'druidic focus']
+	if userClass=='fighter':
+		hitDice=10
+		hpMax+=10
+		proficiences+=['armor','shields','simple weapons','martial weapons']
+		savingThrows=[0,2]
+		skillOne=str(input('Choose a skill from the following: Acrobatics, Animal Handling, Athletics, History, Insight, Intimidation, Perception, or Survival'))
+		skillTwo=str(input('Choose a different skill from the above: '))
+		proficiences+=[skillOne,skillTwo]
+		print('For the next few options, enter 1 to choose the first option, and 2 to choose the second.')
+		equipmentChoice=input('Would you like chain mail or leather armor, a longbow, and 20 arrows? ')
+		if equipmentChoice==1:
+			equipment+=['chain mail']
+		else:
+			equipment+=['leather armor','longbow','twenty arrows']
+		equipmentChoice=input('Would you like a martial weapon and a shield or 2 martial weapons?')
+		if equipmentChoice==1:
+			equipment+=['martial weapons','shield']
+		else:
+			equipment+=['two martial weapons']
+		equipmentChoice=input('Would you like a light crossbow and 20 bolts or two handaxes? ')
+		if equipmentChoice==1:
+			equipment+=['light crossbow', '20 bolts']
+		else:
+			equipment+=['two handaxes']
+		equipmentChoice=input('Would you like a dungeoneer\'s pack or an explorer\'s pack? ')
+		if equipmentChoice==1:
+			equipment+=['dungeoneer\'s pack']
+		else:
+			equipment+=['explorer\'s pack']
+	if userClass=='monk':
+		hitDice=8
+		hpMax+=8
+		proficiences+=['simple weapons']
+		if 'shortsword' not in proficiences:
+			proficiences+=['shortsword']
+		proficiences+=[str(input('Choose a type of artisan\'s tools or a musical instrument to be proficient with: '))]
+		savingThrows=[0,1]
+		skillOne=str(input('Choose a skill from the following: Acrobatics, Athletics, History, Insight, Religion, or Stealth'))
+		skillTwo=str(input('Choose a different skill from the above: '))
+		print('For the next few options, enter 1 to choose the first option, and 2 to choose the second.')
+		equipmentChoice=input('Would you like a shortsword or any simple weapon? ')
+		if equipmentChoice==1:
+			equipment+=['shortsword']
+		else:
+			equipment+=['any simple weapon']
+		equipmentChoice=input('Would you like a dungeoneer\'s pack or an explorer\'s pack? ')
+		if equipmentChoice==1:
+			equipment+=['dungeoneer\'s pack']
+		else:
+			equipment+=['explorer\'s pack']
+		equipment+=['10 darts']
+	if userClass=='paladin':
+		hitDice=10
+		hpMax+=10
+		proficiences+=['armor','shields','simple weapons','martial weapons']
+		savingThrows=[3,5]
+		skillOne=str(input('Choose a skill from the following: Atheltics, Insight, Intimidation, Medicine, Persuasion, or Religion'))
+		skillTwo=str(input('Choose a different skill from the above: '))
+		print('For the next few options, enter 1 to choose the first option, and 2 to choose the second.')
+		equipmentChoice=input('Would you like a martial weapon and a shield or 2 martial weapons?')
+		if equipmentChoice==1:
+			equipment+=['martial weapons','shield']
+		else:
+			equipment+=['two martial weapons']
+		equipmentChoice=input('Would you like five javelins or any simple melee weapon? ')
+		if equipmentChoice==1:
+			equipment+=['light crossbow', '20 bolts']
+		else:
+			equipment+=['two handaxes']
+		equipmentChoice=input('Would you like a priest\'s pack or an explorer\'s pack? ')
+		if equipmentChoice==1:
+			equipment+=['priest\'s pack']
+		else:
+			equipment+=['explorer\'s pack']
+		equipment+=['chain mail','holy symbol']
+	if userClass=='ranger':
+		hitDice=10
+		hpMax+=10
+		if 'light armor' not in proficiences:
+			proficiences+=['light armor']
+		if 'medium armor' not in proficiences:
+			proficiences+=['medium armor']
+		proficiences+=['shields']
+		savingThrows=[0,1]
+		skillOne=str(input('Choose a skill from the following: Animal Handling, Atheltics, Insight, Investigation, Nature, Perception, Stealth, or Survival'))
+		skillTwo=str(input('Choose a different skill from the above: '))
+		skillThree=str(input('Choose a third skill from the above: '))
+		print('For the next few options, enter 1 to choose the first option, and 2 to choose the second.')
+		equipmentChoice=input('Would you like scale mail or leather armor?')
+		if equipmentChoice==1:
+			equipment+=['scale mail']
+		else:
+			equipment+=['leather armor']
+		equipmentChoice=input('Would you like two shortswords or two simple melee weapons? ')
+		if equipmentChoice==1:
+			equipment+=['two shortswords']
+		else:
+			equipment+=['two simple melee weapons']
+		equipmentChoice=input('Would you like a dungeoneer\'s pack or an explorer\'s pack? ')
+		if equipmentChoice==1:
+			equipment+=['dungeoneer\'s pack']
+		else:
+			equipment+=['explorer\'s pack']
+		equipment+=['longbow','quiver of 20 arrows']
+	if userClass=='rogue':
+		hitDice=8
+		hpMax+=8
+		if 'light armor' not in proficiences:
+			proficiences+=['light armor']
+		proficiences+=['simple weapons']
+		if 'hand crossbow' not in proficiences:
+			proficiences+=['hand crossbow']
+		if 'longsword' not in proficiences:
+			proficiences+=['longsword']
+		if 'rapier' not in proficiences:
+			proficiences+=['rapier']
+		if 'shortsword' not in proficiences:
+			proficiences+=['shortsword']
+		proficiences+=['thieves\' tools']
+		savingThrows=[1,4]
+		skillOne=str(input('Choose a skill from the following: Acrobatics, Athletics, Deception, Insight, Intimidation, Investigation, Perception, Performance, Persuasion, Sleight of Hand, or Stealth'))
+		skillTwo=str(input('Choose a different skill from the above: '))
+		skillThree=str(input('Choose a third skill from the above: '))
+		skillFour=str(input('Choose a fourth skill from the above: '))
+		print('For the next few options, enter 1 to choose the first option, 2 to choose the second, and 3 if you want the third')
+		equipmentChoice=input('Would you like a rapier or a shortsword?')
+		if equipmentChoice==1:
+			equipment+=['rapier']
+		else:
+			equipment+=['shortsword']
+		equipmentChoice=input('Would you like a shortbow and 20 arrows or a shortsword? ')
+		if equipmentChoice==1:
+			equipment+=['shortbow', '20 arrows']
+		else:
+			equipment+=['shortsword']
+		equipmentChoice=input('Would you like a burglar\'s pack, a dungeoneer\'s pack, or an explorer\'s pack? ')
+		if equipmentChoice==1:
+			equipment+=['burglar\'s pack']
+		elif equipmentChoice==2:
+			equipment+=['dungeoneer\'s pack']
+		else:
+			equipment+=['explorer\'s pack']
+		equipment+=['leather armor','two daggers','thieves\' tools']
+	if userClass=='sorcerer':
+		hitDice=6
+		hpMax+=6
+		proficiences+=['dagger','dart','sling','quarterstaff','light crossbow']
+		savingThrows=[2,5]
+		skillOne=str(input('Choose a skill from the following: Arcana, Deception, Insight, Intimidation, Persuasion, Religion'))
+		skillTwo=str(input('Choose a different skill from the above: '))
+		print('For the next few options, enter 1 to choose the first option, 2 to choose the second, and 3 if you want the third')
+		equipmentChoice=input('Would you like a light crossbow and 20 bolts or any simple weapon')
+		if equipmentChoice==1:
+			equipment+=['light crossbow','20 bolts']
+		else:
+			equipment+=['any simple weapon']
+		equipmentChoice=input('Would you like a component pouch or an arcane focus? ')
+		if equipmentChoice==1:
+			equipment+=['component pouch']
+		else:
+			equipment+=['arcane focus']
+		equipmentChoice=input('Would you like a dungeoneer\'s pack, or an explorer\'s pack? ')
+		if equipmentChoice==1:
+			equipment+=['dungeoneer\'s pack']
+		else:
+			equipment+=['explorer\'s pack']
+		equipment+=['two daggers']
+	if userClass=='warlock':
+		hitDice=8
+		hpMax+=8
+	if userClass=='wizard':
+		hitDice=6
+		hpMax+=6
 
 
