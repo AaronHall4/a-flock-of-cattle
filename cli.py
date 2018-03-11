cb9ba40ac517e3afc376fdef65608c2c5c12fc7b
Baram Sosis <bsausi42@gmail.com>
2018-03-11 13:53:39 -0400

    Added cli and pretty printing

---
+++ cli.py
@@ -0,0 +1,63 @@
+import pickle
+
+from sklearn.pipeline import Pipeline
+
+import prettyprinters as pp
+import characterCreation as cc
+
+balanced=False
+race_lut = ['dwarf', 'elf', 'halfling', 'human', 'dragonborn', 'gnome', 'half-elf', 'half-orc', 'tiefling']
+class_lut = ['barbarian', 'bard', 'cleric', 'druid', 'fighter', 'monk', 'paladin', 'ranger', 'rogue', 'sorcerer', 'warlock', 'wizard']
+
+
+def main():
+	if balanced:
+		f = open('ml-magic/class_balanced_pipeline', 'rb')
+		class_pipeline = pickle.load(f)
+		f.close()
+		f = open('ml-magic/race_balanced_pipeline', 'rb')
+		race_pipeline = pickle.load(f)
+		f.close()
+	else:
+		f = open('ml-magic/class_unbalanced_pipeline', 'rb')
+		class_pipeline = pickle.load(f)
+		f.close()
+		f = open('ml-magic/race_unbalanced_pipeline', 'rb')
+		race_pipeline = pickle.load(f)
+		f.close()
+		
+	pp.head('Welcome to ')
+
+	pp.description(
+"""This is a tool to help walk beginning players through the process of
+creating their first Dungeons and Dragons character sheet. The first step
+is to think of a backstory for your character. Your backstory should
+describe where your character came from, what motivates them, and what
+their goals and ideals are. We'll try to use your backstory to make some
+suggestions for how to design your character.""")
+
+	backstory = pp.query('Type in your backstory:')
+
+	suggested_race = race_lut[race_pipeline.predict([backstory])[0]]
+	suggested_class = class_lut[class_pipeline.predict([backstory])[0]]
+
+	# cc.characterCreation(suggested_race, suggested_class, backstory)
+
+	pp.description(suggested_class)
+	pp.description(suggested_race)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
---
+++ prettyprinters.py
@@ -0,0 +1,26 @@
+
+from clint.textui import puts, indent, colored, prompt
+
+class bcolors:
+    QUERY = '\033[35m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    ENDBOLD = '\033[22m'
+
+def query(text):
+	res = prompt.query(bcolors.QUERY+text+bcolors.ENDC)
+	return res
+
+def description(text):
+	puts('')
+	with indent(4):
+		puts(colored.green(text))
+	puts('')
+
+def head(text):
+	puts('')
+	with indent(8):
+		puts(colored.blue(bcolors.BOLD+text+bcolors.ENDBOLD))
+	puts('')
+
+
